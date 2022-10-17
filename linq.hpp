d82fc983225e60cb32b9004cdd2ece1e20185f5d
GatoPata <ion699503@gmail.com>
2022-10-17 05:59:44 +0200

    v0.15

--- linq.hpp
+++ linq.hpp
@@ -21,20 +21,20 @@ namespace enhance::linq {
         vector(const std::vector<T> &vec) : std::vector<T>(vec){}
 
         template <typename Result, typename Predicate>
-        vector<Result> select(Predicate pred){
+        vector<Result> select(Predicate pred) const{
             vector<Result> vector;
             for (auto it = this->begin(); it < this->end(); it++) vector.push_back(pred(*it, this->end() - it));
             return vector;
         }
 
-        vector<T> concat(const vector<T> &vec){
+        vector<T> concat(const vector<T> &vec) const{
             vector<T> vector = *this;
             vector.insert(vector.end(), vec.begin(), vec.end());
             return vector;
         }
 
         template <typename Result, typename Second, typename Predicate>
-        vector<Result> zip(const vector<Second> &vec, Predicate pred){
+        vector<Result> zip(const vector<Second> &vec, Predicate pred) const{
             if (vec.count() != count()) throw exception("Vector size is not the same :(");
             vector<Result> vector;
             auto it1 = this->begin();
@@ -44,46 +44,50 @@ namespace enhance::linq {
         }
 
         template <typename Predicate>
-        vector<T> where(Predicate pred){
+        vector<T> where(Predicate pred) const{
             vector<T> vector;
             for (auto it = this->begin(); it < this->end(); it++) if (pred(*it)) vector.push_back(*it);
             return vector;
         }
 
-        vector<T> skip(size_t elements){
+        vector<T> skip(size_t elements) const{
             if (this->begin() + elements >= this->end()) throw exception("Can't skip, too much :(");
             return vector<T>(this->begin() + elements, this->end());
         }
 
-        vector<T> skip_last(size_t elements){
+        vector<T> skip_last(size_t elements) const{
             if (this->begin() >= this->end() - elements) throw exception("Can't skip, too much :(");
             return vector<T>(this->begin(), this->end() - elements);
         }
 
-        vector<T> take(size_t elements){
+        vector<T> take(size_t elements) const{
             if (this->begin() + elements >= this->end()) throw exception("Can't take, too much :(");
             return vector<T>(this->begin(), this->begin() + elements);
         }
 
-        vector<T> take_last(size_t elements){
+        vector<T> take_last(size_t elements) const{
             if (this->begin() >= this->end() - elements) throw exception("Can't take, too much :(");
             return vector<T>(this->end() - elements, this->end());
         }
 
+        vector<T> reverse() const{
+            return vector{this->rbegin(), this->rend()};
+        }
+
         template <typename Result, typename Accumulator, typename Predicate, typename Selector>
-        Result aggregate(Accumulator accumulate, Predicate pred, Selector sel){
+        Result aggregate(Accumulator accumulate, Predicate pred, Selector sel) const{
             for (auto it = this->begin(); it < this->end(); it++) accumulate = pred(accumulate, *it);
             return sel(accumulate);
         }
 
         template <typename Accumulator, typename Predicate>
-        Accumulator aggregate(Accumulator accumulate, Predicate pred){
+        Accumulator aggregate(Accumulator accumulate, Predicate pred) const{
             for (auto it = this->begin(); it < this->end(); it++) accumulate = pred(accumulate, *it);
             return accumulate;
         }
 
         template <typename Predicate>
-        T aggregate(Predicate pred){
+        T aggregate(Predicate pred) const{
             if (this->begin() == this->end()) throw exception("No elements :(");
             T accumulate = *this->begin();
             for (auto it = this->begin()+1; it < this->end(); it++) accumulate = pred(accumulate, *it);
@@ -91,31 +95,31 @@ namespace enhance::linq {
         }
 
         template <typename Predicate = bool(const T&)>
-        T first(Predicate pred = [](const T &){return true;}){
+        T first(Predicate pred = [](const T &){return true;}) const{
             for (auto it = this->begin(); it < this->end(); it++) if (pred(*it)) return *it;
             throw exception("There was no first :(");
         }
 
         template <typename Predicate = bool(const T&)>
-        T first_or_default(Predicate pred = [](const T &){return true;}, T def = {}){
+        T first_or_default(Predicate pred = [](const T &){return true;}, T def = {}) const{
             for (auto it = this->begin(); it < this->end(); it++) if (pred(*it)) return *it;
             return def;
         }
 
         template <typename Predicate = bool(const T &)>
-        T last(Predicate pred = [](const T &){return true;}){
+        T last(Predicate pred = [](const T &){return true;}) const{
             for (auto it = this->rbegin(); it < this->rend(); it++) if (pred(*it)) return *it;
             throw exception("There was no last :(");
         }
 
         template <typename Predicate = bool(const T &)>
-        T last_or_default(Predicate pred = [](const T &){return true;}, T def = {}){
+        T last_or_default(Predicate pred = [](const T &){return true;}, T def = {}) const{
             for (auto it = this->rbegin(); it < this->rend(); it++) if (pred(*it)) return *it;
             return def;
         }
 
         template <typename Result, typename Predicate = Result(const T &)>
-        Result min(Predicate pred = [](const T &a){return a;}){
+        Result min(Predicate pred = [](const T &a){return a;}) const{
             if (this->begin() == this->end()) throw exception("No elements :(");
             Result min = pred(*this->begin());
             for (auto it = this->begin()+1; it < this->end(); it++) {
@@ -125,7 +129,7 @@ namespace enhance::linq {
             return min;
         }
 
-        T min(){
+        T min() const{
             if (this->begin() == this->end()) throw exception("No elements :(");
             T min = *this->begin();
             for (auto it = this->begin()+1; it < this->end(); it++) if (*it < min) min = *it;
@@ -133,7 +137,7 @@ namespace enhance::linq {
         }
 
         template <typename Result, typename Predicate = Result(const T &)>
-        Result max(Predicate pred = [](const T &a){return a;}){
+        Result max(Predicate pred = [](const T &a){return a;}) const{
             if (this->begin() == this->end()) throw exception("No elements :(");
             Result max = pred(*this->begin());
             for (auto it = this->begin()+1; it < this->end(); it++) {
@@ -143,31 +147,31 @@ namespace enhance::linq {
             return max;
         }
 
-        T max(){
+        T max() const{
             if (this->begin() == this->end()) throw exception("No elements :(");
             T max = *this->begin();
             for (auto it = this->begin()+1; it < this->end(); it++) if (*it > max) max = *it;
             return max;
         }
 
-        bool contains(T value){
+        bool contains(T value) const{
             return any([&value](const T &v){return v == value;});
         }
 
         template <typename Predicate = bool(const T &)>
-        bool any(Predicate pred = [](const T &){return true;}){
+        bool any(Predicate pred = [](const T &){return true;}) const{
             for (auto it = this->begin(); it < this->end(); it++) if (pred(*it)) return true;
             return false;
         }
 
         template <typename Predicate>
-        bool all(Predicate pred){
+        bool all(Predicate pred) const{
             for (auto it = this->begin(); it < this->end(); it++) if (!pred(*it)) return false;
             return true;
         }
 
         template <typename Predicate = bool(const T &)>
-        size_t count(Predicate pred = [](const T &){return true;}){
+        size_t count(Predicate pred = [](const T &){return true;}) const{
             size_t count = 0;
             for (auto it = this->begin(); it < this->end(); it++) if (pred(*it)) count++;
             return count;
